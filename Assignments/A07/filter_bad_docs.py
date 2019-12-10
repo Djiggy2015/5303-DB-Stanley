8d0116c33fd5d34db1c2e092b4f8da3474fd59b3
Djiggy2015 <49460397+Djiggy2015@users.noreply.github.com>
2019-12-09 21:41:30 -0600

    Create filter_bad_docs.py

---
+++ Assignments/A07/filter_bad_docs.py
@@ -0,0 +1,35 @@
+"""
+This file cleans a collection and removes and lat/lon pairs that are not numbers
+allowing us to create a spatial index and do distance queries.
+"""
+import pymongo  # package for working with MongoDB
+
+client = pymongo.MongoClient("mongodb://192.168.99.100:27017")
+db = client["armageddon"]
+
+volcanos = db["volcanos"] # Collection name goes here
+
+
+count = 0
+
+for obj in volcanos.find():
+    mongo_id = obj["_id"]
+    lat = obj["latitude"]
+    lon = obj["longitude"]
+    # Check if lat and lon are ints or floats
+    lati = isinstance(lat, int)
+    loni = isinstance(lon, int) 
+    flat = isinstance(lat, float)
+    flon = isinstance(lon, float)
+    
+    # If the lat lons ARE numbers, insert it
+    if (lati or flat) and (flat or flon):
+        volcanos.update_one({'_id':mongo_id}, {"$set": {"loc" : { "type": "Point", "coordinates": [ lon, lat ] }}}, upsert=False)
+    else:
+        print(f"Removing: {lat},{lon}")
+        volcanos.remove({'_id':mongo_id})
+        count += 1
+        
+        
+
+print(f"Count not inserted: {count}")
